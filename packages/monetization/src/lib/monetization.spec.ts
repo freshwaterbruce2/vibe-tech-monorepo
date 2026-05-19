7b31ed7f73f28f71f93882801f195a0a82253f26
freshwaterbruce2 <freshwaterbruce2@gmail.com>
2026-05-19 19:29:44 -0400

    chore: scaffold workstream a packages via clean worktree

---
+++ packages/monetization/src/lib/monetization.spec.ts
@@ -0,0 +1,7 @@
+import { monetization } from './monetization';
+
+describe('monetization', () => {
+  it('should work', () => {
+    expect(monetization()).toEqual('monetization');
+  });
+});
---
+++ packages/monetization/src/lib/monetization.ts
@@ -0,0 +1,3 @@
+export function monetization(): string {
+  return 'monetization';
+}
